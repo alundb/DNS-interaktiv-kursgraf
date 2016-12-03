4a8eff5d66292e107ca8c902a6114b904fab2ac4
unknown <adrlun@student.chalmers.se>
2016-12-04 00:23:15 +0100

    Fix dropdown menu position while zooming. Fix link names in menu content and their destination

--- funkis.js
+++ funkis.js
@@ -1,46 +1,80 @@
+
 $("g").click(function(evt) {
-if ($(this).attr("id")  != "graph0") {
 	var e = evt.target;
-    	var dim = e.getBoundingClientRect();
-    	var x = evt.clientX - dim.left;
-    	var y = evt.clientY - dim.top;
-    	alert("x: "+x+" y:"+y);
-       alert($(this).attr("id"));  
+if ($(this).attr("id")  != "graph0") {
+
+	 var offset = $( this ).offset();
+	// var sth = (this).getBoundingClientRect();
+	//alert("current x: " + offset.left + "current y: " + offset.top);
+	// var bbox = (this).getBBox();
+	// middleX = bbox.x + (bbox.width / 2),
+	// middleY = bbox.y + (bbox.height / 2);
+	// var dim = e.getBoundingClientRect();
+    	//var dim = e.getBoundingClientRect();
+    	//var x = evt.clientX - dim.left;
+    	//var y = evt.clientY - dim.top;
+    	//alert("x: "+evt.cx+" y:"+evt.clientY );
+       //alert($(this).attr("id"));
 	//$(this).text("snabelpabel");
 	//myFunction();
-var elem = document.getElementById("myDropdown");
-$('.dropdown').css('position', 'absolute'); // = evt.clientX ;
-$('.dropdown').css('top',dim.top-5);
-$('.dropdown').css('left',dim.left);
-$('.dropdown').css('left',dim.left);
-//$elem.style.left = evt.clientY;
-	myFunctione() // argument = linkToCoursePage
+//var elem = document.getElementById("myDropdown");
+
+
+// $('.dropdown').css('position', 'absolute'); // = evt.clientX ;
+ $('.dropdown').css('top', offset.top);
+ $('.dropdown').css('left', offset.left);
+var courseName = $(this)[0].childNodes[0].textContent;
+console.log($(this)[0].childNodes[0].textContent);
+//console.log($(this)[0].childNodes[3].textContent);
+//console.log((document.getElementById("node18")).childNodes[3]);
+var link = document.getElementById("myDropdown");
+
+$('.dropdown').css('top', offset.top);
+$('.dropdown').css('left', offset.left);
+link.style.display = 'block'
+
+
+if (link.classList.visibility == "hidden"){
+	link.classList.toggle("show");
+	var elem = document.getElementById("minknapp");
+	elem.style.visibility = "hidden";
+}
+
+document.getElementById("mylink1").setAttribute("href", "/Kursutveckling/" + courseName + ".html");
+
+
+document.getElementById("mylink2").setAttribute("href", map[courseName]);
 
 }
 });
+
+
+
 function myFunction() {
-var popup = document.getElementById('myPopup');
-	if (popup.classList.visibility == "hidden"){
-	popup.classList.toggle('show');
-}
- //window.open('http://www.google.com');
+ window.open(map['fysik']);
 
 }
 
-function myFunctione() {
-    var link = document.getElementById("myDropdown");
-	$(document.getElementById("myDropdown")).attr("href", "http://the.new.url");
-	link.setAttribute("text", "eeee");
-	link.classList.toggle("show");
-var elem = document.getElementById("minknapp");
-elem.style.visibility = "hidden";
-	 //('visibility', hidden);
-	
-    //document.getElementById("myDropdown").classList.toggle("show");
-}   
+function toggleMenu() {
+var link = document.getElementById("myDropdown");
+	//alert("hello");
+	if (link.style.display !== 'none'){
+			link.style.display = 'none'
+	}
+	else {
+		link.style.display = 'block'
+	}
+}
+
+
 function f1(){
-    document.getElementById("mylink1").setAttribute("href", "xyz.php");
+
+    document.getElementById("mylink2").setAttribute("href", "loopes.php");
   }
-
-  
-
+	var linkPre ="https://student.portal.chalmers.se/sv/chalmersstudier/minkursinformation/Sidor/SokKurs.aspx?course_id="
+	var map= {
+	    'fysik': linkPre + "24421&parsergrp=3",
+	    'el': linkPre + "24725&parsergrp=3",
+	    'map_name_3': "hej2",
+	    'map_name_4':"hej3"
+	    }
