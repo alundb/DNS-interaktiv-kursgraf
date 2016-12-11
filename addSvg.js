98ee6f0c45041ab25118f5a7eed6ebd447126298
unknown <adrlun@student.chalmers.se>
2016-12-11 17:33:38 +0100

    Make kursgraf.dot sameheaded,sameedged and concentrated without messing up the graph. Optimize mouse listeners

--- addSvg.js
+++ addSvg.js
@@ -1,21 +1,3 @@
 graphviz_text =  d3.text("kursgraf.dot", function(error, text){
-	//console.log(text);
 	document.body.innerHTML += Viz(text, "svg");
-
-	//var z = Viz(text, "svg");
-	//console.log(z);
-	//wp.appendChild(svg);
-	 var wp = document.getElementById('wrapper');
-	 var z = document.createElement('div');
-
-	 z.setAttribute("id", "testDiv");
-	 wp.appendChild(z);
-
-	// var wp = document.getElementById('wrapper');
-	//
-	//
-	// wp.innerHTML = Viz(text, "svg");
-	//wp.appendChild(z);
-
-	//wp.appendChild(Viz(text, "svg"));
 });
--- funkis.js
+++ funkis.js
@@ -1,84 +1,33 @@
-var menu = [
-	{
-		title: 'Kursutveckling',
-		action: function(elm, d, i) {
-			var courseName = $(elm)[0].childNodes[0].textContent;
-			console.log('Item #1 clicked!');
-			console.log('The data for this circle is: ' + courseName);
 
-		}
-	},
-	{
-		title: 'Kursplan',
-		action: function(elm, d, i) {
-			var courseName = $(elm)[0].childNodes[0].textContent;
-			console.log('You have clicked the second item!');
-			console.log('The data for this circle is: ' + map[courseName]);
-			window.open(map[courseName]);
-		}
-	}
-]
+graphviz_text =  d3.text("kursgraf.dot", function(text){
 
-//graphviz_text =  "digraph { a -> b; }";
-// graphviz_text =  d3.text("kursgraf.dot", function(error, text){
-// 	//console.log(text);
-// 	document.body.innerHTML += Viz(text, "svg");
-// });
-
-//var txtFile = kaffe.txt;
+	document.body.innerHTML += Viz(text, "svg");
+});
 
 
-function waitSeconds(iMilliSeconds) {
-    var counter= 0
-        , start = new Date().getTime()
-        , end = 0;
-    while (counter < iMilliSeconds) {
-        end = new Date().getTime();
-        counter = end - start;
-    }
-}
-
-waitSeconds(500); //Delay is needed, otherwise window.onload wont have time to notice the change. 
-//
+setTimeout(function addListeners(){
+  var x = d3.selectAll('g');
+  console.log(x);
+  x.on("mousedown", d3.contextMenu(menu)); //Add contextmenu to all nodes and eges.
+  var p =document.getElementById("graph0");
+  d3.select(p).on("mousedown", ""); //Remove contextMenu menu feuture from master element.
+}, 0);
 
 
 
-//document.body.innerHTML += Viz(graphviz_text, "svg");
 
-//
-// var x = d3.selectAll('g');
-// console.log(x);
 
- //x.on("mousedown", d3.contextMenu(menu)); //Add contextmenu to all nodes and eges.
- //x.on("contextmenu", d3.contextMenu(menu)); //Add contextmenu to all nodes and eges.
- //var p =document.getElementById("graph0");
- //d3.select(p).on("mousedown", ""); //Remove contextMenu menu feuture from master element.
 
-// x.on("mousedown", function (d){
-// d3.contextMenu(menu);
-//
+// $("g").click(function(evt) {
+//   //if ($(this).attr("id")  != "graph0") {
+//  			alert("hello");
+//  		//$("g").on("mousedown",  d3.contextMenu(menu));
+//  		//d3.contextMenu(menu);
+//  	//}
 // });
 
 
- // $("wrapper").click(function(evt) {
- //  //if ($(this).attr("id")  != "graph0") {
- // 			alert("hello");
- // 		//$("g").on("mousedown",  d3.contextMenu(menu));
- // 		//d3.contextMenu(menu);
- // 	//}
- // });
 
-
- //Add contextmenu to all nodes and eges.
-// var p =document.getElementById("graph0");
-// d3.select(p).on("mousedown", ""); //Remove contextMenu menu feuture from master element.
-
-
-
-
-//d3.selectAll('g').on("contextmenu", d3.contextMenu(menu));
-// var p =document.getElementById("node1").childNodes[0];
-// d3.select(p).on("contextmenu", d3.contextMenu(menu));
 // $("g").click(function(evt) {
 // 	var e = evt.target;
 //
@@ -100,44 +49,52 @@ waitSeconds(500); //Delay is needed, otherwise window.onload wont have time to n
 //        //alert($(this).attr("id"));
 // 	//$(this).text("snabelpabel");
 // 	//myFunction();
-// //var elem = document.getElementById("myDropdown");
-//
-//
-// // $('.dropdown').css('position', 'absolute'); // = evt.clientX ;
-//  $('.dropdown').css('top', offset.top);
-//  $('.dropdown').css('left', offset.left);
 // var courseName = $(this)[0].childNodes[0].textContent;
 // console.log($(this)[0].childNodes[0].textContent);
 // //console.log($(this)[0].childNodes[3].textContent);
 // //console.log((document.getElementById("node18")).childNodes[3]);
-// var link = document.getElementById("myDropdown");
-//
-// $('.dropdown').css('top', offset.top);
-// $('.dropdown').css('left', offset.left);
-// link.style.display = 'block'
-//
-//
-// if (link.classList.visibility == "hidden"){
-// 	link.classList.toggle("show");
-// 	var elem = document.getElementById("minknapp");
-// 	elem.style.visibility = "hidden";
-// }
-//
-// document.getElementById("mylink1").setAttribute("href", "/Kursutveckling/" + courseName + ".html");
-//
-//
-// document.getElementById("mylink2").setAttribute("href", map[courseName]);
-//
-// }
-// });
 
 
 
+var menu = [
+	{
+		title: 'Kursutveckling',
+		action: function(elm, d, i) {
+			var courseName = $(elm)[0].childNodes[0].textContent;
+			console.log('Item #1 clicked!');
+			console.log('The data for this circle is: ' + courseName);
+			window.open(dnslink + courseName + "/Kursutveckling");
 
-var linkPre ="https://student.portal.chalmers.se/sv/chalmersstudier/minkursinformation/Sidor/SokKurs.aspx?course_id="
-var map= {
-	  'fysik': linkPre + "24421&parsergrp=3",
-	  'el': linkPre + "24725&parsergrp=3",
+		}
+	},
+	{
+		title: 'Kursrelationer',
+		action: function(elm, d, i) {
+			var courseName = $(elm)[0].childNodes[0].textContent;
+			console.log('You have clicked the second item!');
+			console.log('The data for this circle is: ' + portMap[courseName]);
+			window.open(dnslink + courseName + "/Kursrelationer");
+		}
+	},
+	{
+		title: 'Kursplan',
+		action: function(elm, d, i) {
+			var courseName = $(elm)[0].childNodes[0].textContent;
+			console.log('You have clicked the second item!');
+			console.log('The data for this circle is: ' + portMap[courseName]);
+			window.open(portMap[courseName]);
+		}
+	}
+]
+
+
+var portlink ="https://student.portal.chalmers.se/sv/chalmersstudier/minkursinformation/Sidor/SokKurs.aspx?course_id="
+var portMap= {
+	  'fysik': portlink + "24421&parsergrp=3",
+	  'el': portlink + "24725&parsergrp=3",
 	  'map_name_3': "hej2",
 	  'map_name_4':"hej3"
-	  }
+		}
+
+
+var dnslink ="https://dns.dtek.se/course_id="
